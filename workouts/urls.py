cc12e5390d07980f9b65616d7b3d438c39381a8d
Alexis Sammut <alexis.sammut26@gmail.com>
2025-11-25 18:14:21 +0000

    create workout views

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
+from .views import (
+    WorkoutListView,
+    WorkoutDetailView,
+    WorkoutCreateView,
+    WorkoutUpdateView,
+    WorkoutDeleteView,
+)
 
 urlpatterns = [
-    path('log/', views.log, name='log-workout'),
-    path('review/',views.review, name='review-workouts'),
+    path("log/", WorkoutCreateView.as_view(), name="log-workout"),
+    path("review/", views.review, name="review-workouts"),
+    path("list/", WorkoutListView.as_view(), name="workouts-list"),
+    path("<int:pk>/", WorkoutDetailView.as_view(), name="workout-detail"),
+    path("<int:pk>/update/", WorkoutUpdateView.as_view(), name="workout-update"),
+    path('<int:pk>/delete/', WorkoutDeleteView.as_view(), name='workout-delete'),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,11 +1,81 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Workout
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    )
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
 
-def log(request):
-    return render(request, 'workouts/log_workout.html')
 
+@login_required
 def review(request):
-    context = {
-       'workouts': Workout.objects.all()
-    }
-    return render(request, 'workouts/review_workouts.html', context) 
+    context = {"workouts": Workout.objects.all()}
+    return render(request, "workouts/review_workouts.html", context)
+
+
+class WorkoutListView(LoginRequiredMixin, ListView):
+    model = Workout
+    context_object_name = "workouts"
+    ordering = ["-date"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            user=self.request.user
+        )  # Filter the queryset to only include objects created by the current user
+
+
+class WorkoutDetailView(LoginRequiredMixin, DetailView):
+    model = Workout
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            user=self.request.user
+        )  # Add a filter to ensure the object also belongs to the current user
+
+
+class WorkoutCreateView(LoginRequiredMixin, CreateView):
+    model = Workout
+    template_name = "workouts/post_workout.html"
+    fields = ["workout_type", "duration", "distance", "pace", "intensity", "calories"]
+
+    def form_valid(self, form):
+        """Automatically set the user to the logged-in user"""
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """
+        Returns the URL to redirect to after successful form submission.
+        """
+        return reverse("workout-detail", kwargs={"pk": self.object.pk})
+
+
+class WorkoutUpdateView(LoginRequiredMixin, UpdateView):
+    model = Workout
+    fields = ["workout_type", "duration", "distance", "pace", "intensity", "calories"]
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user 
+        return super().form_valid(form)
+
+class WorkoutDeleteView(LoginRequiredMixin, DeleteView): 
+    model = Workout
+    success_url = reverse_lazy('workouts-list')
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+    
+    
